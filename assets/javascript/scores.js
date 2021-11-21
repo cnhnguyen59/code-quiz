fd7eba09df275b455c493fcfdbaf96957f4da2e8
Christine Nguyen <cnhnguyen59@gmail.com>
2021-11-20 19:55:31 -0500

    created second html fand js files for scores page

---
+++ assets/javascript/scores.js
@@ -0,0 +1,7 @@
+const tryBtn = document.querySelector('.try-again');
+
+
+tryBtn.addEventListener('click', () => {window.location.href = 'index.html'})
+
+
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,32 @@
 
 const startBtn = document.querySelector('.start-button')
+const saveBtn = document.querySelector('.save')
 const timerEl = document.querySelector('.timer-count')
 const scoreEl = document.querySelector('.user-score')
 const questionEl = document.querySelector('.question')
 const answersEl = document.querySelector('.answer-buttons')
+const finalScoreEl = document.querySelector('.final-score')
+const input = document.querySelector('.input-box')
+const allScoresEl = document.querySelector('.all-scores')
+
+/* const oneInit = document.getElementById('oneInit');
+const oneScore = document.getElementById('oneScore');
+const twoInit = document.getElementById('twoInit');
+const twoScore = document.getElementById('twoScore');
+const threeInit = document.getElementById('threeInit');
+const threeScore = document.getElementById('threeScore');
+var inputBox = document.getElementById('input');
+const againBtn = document.getElementById('again-btn') */
 
 
 var timer
 var timerCount = 60
 var score = 0
+var allScores = localStorage.getItem("allScores")
+
 
 startBtn.addEventListener('click', startQuiz)
+saveBtn.addEventListener('click', save)
 
 
 function startQuiz() {
@@ -25,16 +41,13 @@ function startQuiz() {
 function startTimer(){
     timer = setInterval(function(){
         timerCount--;
-        timerEl.textContent =timerCount
-
+        timerEl.textContent = timerCount
         if (timerCount === 0){
             window.alert("Time's Up!")
             endQuiz()
         }
 
     }, 1000)
-
-       
 }
 
 function getQ() {
@@ -82,13 +95,14 @@ function selectedAnswer(e) {
     }
 }
 
-
 function endQuiz() {
-    //stops timer
     clearInterval(timer)
-    //saves score
-    localStorage.setItem('mostRecentScore', score)
-    //goes to highscores list
+    finalScoreEl.textContent = score
+    document.getElementById('qb').style.visibility = 'hidden';
+    document.getElementById('sb').style.visibility = 'visible';
 }
 
-
+function save(){
+    //store scores in local storage and write to scores list
+    location.href = 'scores.html'
+}
